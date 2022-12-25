365cc45879e3325280496a4bed7ba6c9e7d2eab8
William Baele <baelewilliam1999@gmail.com>
2022-12-25 09:28:33 +0800

    fixes

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,5 +1,18 @@
 class SneakersController < ApplicationController
   require 'date'
   def index
+    @sneakers = Sneaker.all
+  end
+
+  def show
+    @sneaker = Sneaker.find(params[:id])
+  end
+  def new
+    @sneaker = Sneaker.new
+  end
+
+  private
+  def sneaker_params
+    params.require(:sneaker).permit(:name, :sku, :drop_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resource :sneakers
+
 end
