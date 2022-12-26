515e3d08baba3dea285760cdc1527a52950e3770
William Baele <baelewilliam1999@gmail.com>
2022-12-26 17:25:51 +0100

    controller fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  require 'date'
+
   def index
     @sneakers = Sneaker.all
+  end
+
+  private
+  def sneaker_params
+    params.require(:sneaker).permit(:name, :sku, :drop_date, :description, :estimated_ressell, :model, :brand, :ressell_circle)
   end
 end
--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,11 +1,21 @@
 class SneakersController < ApplicationController
-  require 'date'
   def index
     @sneakers = Sneaker.all
   end
 
   def show
-    @sneaker = Sneaker.find(params[:id])
+    if Sneaker.exists?(params[:id])
+      @sneaker = Sneaker.find(params[:id])
+    else
+      redirect_to root_path
+      flash[:message] = "Something went wrong"
+    end
+  end
+  def create
+    @sneaker = Sneaker.new(sneaker_params)
+    if @sneaker.save!
+      redirect_to sneaker_path(@sneaker)
+    end
   end
   def new
     @sneaker = Sneaker.new
@@ -13,6 +23,6 @@ class SneakersController < ApplicationController
 
   private
   def sneaker_params
-    params.require(:sneaker).permit(:name, :sku, :drop_date)
+    params.require(:sneaker).permit(:name, :sku, :drop_date, :description, :estimated_ressell, :model, :brand)
   end
 end
