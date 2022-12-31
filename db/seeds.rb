43ac565543f20bd84d0d1331e08a9aec3705f1bd
William Baele <baelewilliam1999@gmail.com>
2022-12-31 16:57:02 +0100

    added rating symbols on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ sku = ["DD1509-100","DD1509-220", "DD1519-100", "MP1509-100"]
 drop_date = ["2022-10-10","2022-10-11","2022-10-12","2022-10-13"]
 
 40.times do
-  sneaker = Sneaker.create!(name: Faker::Name.middle_name, sku: sku.sample, drop_date: drop_date.sample, description: Faker::Lorem.sentence(word_count: 20), retail: rand(100..300))
+  sneaker = Sneaker.create!(name: Faker::Name.middle_name, sku: sku.sample, drop_date: drop_date.sample, description: Faker::Lorem.sentence(word_count: 20), retail: rand(100..300), estimated_ressell: rand(1..5))
   puts sneaker.name
 end
